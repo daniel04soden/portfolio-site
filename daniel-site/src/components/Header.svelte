e790dbc1c7fb1cd47401ced89ccf0d3f411c0769
Daniel Soden <dsoden09@gmail.com>
2025-08-13 21:41:13 +0100

    created file structure, page svelte files and starter header

---
+++ daniel-site/src/components/Header.svelte
@@ -0,0 +1,7 @@
+<header>
+  <div>
+<h1>Daniel Soden, Who am I?</h1>
+    <img src="../lib/assets/dan.jpeg" alt="Daniel Soden Smiling in a white shirt">
+    <h2>Welcome to my Portfolio Website! (Crafted in Svelte)</h2>
+</div>
+</header>
